3d0dfadbf589018673e863a7839c393c765af93f
yeayeah <58143397+yeayeah@users.noreply.github.com>
2022-02-16 21:42:27 +0100

    file_get_contents: nasty fix

--- derpbot/misc.py
+++ derpbot/misc.py
@@ -14,11 +14,22 @@ def file_get_contents(uri, postdata=None, proxies=None):
 	http = RsHttp(host,ssl=ssl,port=port, keep_alive=True, follow_redirects=True, auto_set_cookies=True, proxies=proxies, user_agent='Mozilla/5.0 (Windows NT 6.1; rv:60.0) Gecko/20100101 Firefox/60.0')
 	if not http.connect(): return None
 
-	if postdata is None: hdr, res = http.get(uri)
-	else: hdr, res = http.post(uri, postdata)
+	try:
+		if postdata is None: hdr, res = http.get(uri)
+		else: hdr, res = http.post(uri, postdata)
 
-	if isinstance(res, unicode): res = res.encode('utf-8')
-	return res
+		if isinstance(res, unicode): res = res.encode('utf-8')
+		return res
+	except Exception as e:
+		if hasattr(e, 'message'): print('file_get_contents(error): %s' %e.message)
+		else: print('file_get_contents(error): %s' %message)
 
 def isop(self, chan, nick):
 	return True if 'op' in self.nicklist[chan][nick] else False
+
+def is_ignored_str(self, chan, string):
+	try: ignores = self.settings[chan]['ignore']
+	except: return False
+	for ignore in ignores:
+		if hasattr(ignore, 'string') and re.match(ignore['string'], string): return True
+	return False
