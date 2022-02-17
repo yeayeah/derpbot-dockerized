e442e028a95858bf424b1ac303aefe98f3ad7ec1
yeayeah <58143397+yeayeah@users.noreply.github.com>
2022-02-17 01:59:56 +0100

    fix weird bug

--- derpbot/misc.py
+++ derpbot/misc.py
@@ -13,17 +13,14 @@ def nickmask(data):
 def file_get_contents(uri, postdata=None, proxies=None):
 	host, port, ssl, uri = _parse_url(uri)
 	http = RsHttp(host,ssl=ssl,port=port, keep_alive=True, follow_redirects=True, auto_set_cookies=True, proxies=proxies, user_agent='Mozilla/5.0 (Windows NT 6.1; rv:60.0) Gecko/20100101 Firefox/60.0')
-	if not http.connect(): return None
+	try: http.connect()
+	except Exception as e: return None
 
-	try:
-		if postdata is None: hdr, res = http.get(uri)
-		else: hdr, res = http.post(uri, postdata)
+	if postdata is None: hdr, res = http.get(uri)
+	else: hdr, res = http.post(uri, postdata)
 
-		if isinstance(res, unicode): res = res.encode('utf-8')
-		return res
-	except Exception as e:
-		if hasattr(e, 'message'): print('file_get_contents(error): %s' %e.message)
-		else: print('file_get_contents(error): %s' %message)
+	if isinstance(res, unicode): res = res.encode('utf-8')
+	return res
 
 def isop(self, chan, nick):
 	return True if 'op' in self.nicklist[chan][nick] else False
