b6d202c000c9826b4a6bab39f5b01160627acb7a
yeayeah <58143397+yeayeah@users.noreply.github.com>
2022-03-06 00:06:00 +0100

    plugins/memo: fixbug, make it work

--- derpbot/plugins/memo/__init__.py
+++ derpbot/plugins/memo/__init__.py
@@ -2,7 +2,6 @@ import time
 import hashlib
 from misc import nickmask
 
-
 def event_memo(arr):
 	self = arr['self']
 	if not hasattr(self, 'p_tell'): return None
@@ -28,38 +27,41 @@ def action_memo(arr):
 	if arr['command'] == 'provides': return ['tell', 'remind', 'inbox']
 
 	self = arr['self']
+	chan = arr['chan']
+	nick = arr['nick']
 
 	if arr['command'] == 'tell':
 		if not hasattr(self, 'p_tell'): self.p_tell = dict()
 
-		nick_from = arr['nick']
+		nick_from = nick
 		nick_to = arr['args'][0]
 		message = ' '.join( arr['args'][1:] )
-		md5sum = hashlib.md5(message).hexdigest()
+		md5sum = hashlib.md5('%s:%s:%s' % (nick_from, nick_to, message)).hexdigest()
 
-		self.p_tell[md5sum] = {
-			'nick_to': nick_to,
-			'nick_from': nick_from,
-			'message': message,
-			'ticks': time.time()
-		}
+		if not md5sum in self.p_tell:
+			self.p_tell[md5sum] = {
+				'nick_to': nick_to,
+				'nick_from': nick_from,
+				'message': message,
+				'ticks': time.time()
+			}
 
-		return {'reply': 'Message saved.' %md5sum, 'self': self }
+			return {'reply': '%s: message saved.' %nick, 'self': self }
 	
 	elif arr['command'] == 'remind':
 		if not hasattr(self, 'p_remind'): self.p_remind = dict()
 
 		factors = [ 1, 60, 60*60, 60*60*24 ]
 		message = ' '.join( arr['args'][1:] )
-		md5sum = hashlib.md5('%s:%s' % (arr['nick'], message)).hexdigest()
 		delay = 0
 
 		d_split = arr['args'][0].split(':')
 		d_split.reverse()
+
 		for i in range(len(d_split)):
-			j = d_split[i] * factors[i]
+			j = int(d_split[i]) * int(factors[i])
 			delay += j
 
-		self.irc.privmsg(chan, message, delay)
+		self.irc.privmsg(chan, '%s: %s' %(nick,message), delay)
 
-		return {'reply': 'Reminder saved.' %md5sum, 'self': self }
+		return {'reply': '%s: reminder saved.' %nick, 'self': self }
