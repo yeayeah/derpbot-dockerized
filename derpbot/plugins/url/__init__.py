914a5cb6b86ba8c84b4c12073a4bdf05113beefa
Your Name <you@example.com>
2022-06-12 02:44:06 +0200

    plugins:uri: run "passive" checks in a thread thingy

--- derpbot/plugins/url/__init__.py
+++ derpbot/plugins/url/__init__.py
@@ -3,6 +3,7 @@ import re
 import time
 import random
 import misc
+import threading
 
 def _get_url_title(uri, proxies=None):
 	def _format(string):
@@ -32,14 +33,26 @@ def _update_self(self, chan):
 	if not 'p_url' in self.settings[chan]: self.settings[chan]['p_url'] = []
 	return self
 
+def __process_to_chan(uri, chan, self):
+	title, desc = _get_url_title(uri, proxies=self.args.http_proxy)
+
+	if desc is None and title is None: return
+	elif nuri is not None: self.irc.privmsg(chan, nuri)
+
+	j = title if title is not None else desc
+	self.irc.privmsg(chan, '^ %s' %j)
+
 def event_url(arr):
 	if arr['event'] != 'PRIVMSG': return None
+
 	self = arr['self']
 	if self.irc.nick != self.irc.mynick: return None
+
 	split = arr['recv'].split(' ')
 	chan = split[2]
 	self = _update_self(self, chan)
 	line =  ' '.join( split[3:] ).lstrip(':')
+
 	for uri in line.split(' '):
 		if uri.find('://') == -1: continue
 		domain = uri.split('/')[2]
@@ -52,14 +65,7 @@ def event_url(arr):
 		if skip: continue
 		nuri = misc._inspect_uri(uri)
 		check = nuri if nuri is not None else uri
-		title, desc = _get_url_title(check, proxies=self.args.http_proxy)
-
-		if desc is None and title is None: continue
-		elif nuri is not None: self.irc.privmsg(chan, nuri)
-
-		j = title if title is not None else desc
-		self.irc.privmsg(chan, '^ %s' %j)
-		time.sleep(0.5)
+		threading.Timer(0, __process_to_chan, args=(uri=check, chan=chan, self=self)).start()
 
 def action_url(arr):
 	provides = ['title', 'ud', 'ignore']
