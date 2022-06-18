d10d9495f6ec4ff12c3f04931bcaf9069d2f75c9
Your Name <you@example.com>
2022-06-18 23:59:06 +0200

    plugins:opmode make use of new users.py file

--- derpbot/plugins/opmodes/__init__.py
+++ derpbot/plugins/opmodes/__init__.py
@@ -7,26 +7,38 @@ def event_opmodes(arr):
 		split = arr['recv'].split(' ')
 		chan = split[2].lstrip(':')
 		nick, mask = misc.nickmask(split[0])
-		access = users.get_user_access(self, mask)
-		if access is None: return
-		elif access <= 10: self.irc.send('MODE %s +o %s' % (chan,nick))
-		elif access <= 20: self.irc.send('MODE %s +v %s' % (chan,nick))
+		if nick == self.irc.nick: return
+		access = users.get_chan_access(self, chan, mask)
+		if access is not None:
+			if access <= 10: self.irc.send('MODE %s +o %s' % (chan,nick))
+			elif access <= 20: self.irc.send('MODE %s +h %s' % (chan,nick))
+			elif access <= 30: self.irc.send('MODE %s +v %s' % (chan, nick))
 
 def action_opmodes(arr):
-	if arr['command'] == 'provides': return [ 'op', 'deop', 'voice', 'devoice', 'ban', 'deban', 'topic' ]
+	if arr['command'] == 'provides': return [ 'op', 'deop', 'voice', 'devoice', 'ban', 'deban', 'topic', 'up', 'down' ]
 
 	self = arr['self']
+	chan = arr['chan']
+	nick = arr['nick']
+	mask = arr['mask']
 
-	# if user doesnt have op access
-	if not misc.isop(self, arr['chan'], arr['nick']):
-		access = users.get_user_access(self, arr['mask'])
-		if access > 10: return None
+	if arr['command'] == 'up' or arr['command'] == 'down':
+		access = users.get_chan_access(self, chan, mask)
+		if access is not None:
+			sign = '+' if arr['command'] == 'up' else '-'
+			if access <= 10: self.irc.send('MODE %s %so %s' % (chan, sign, nick))
+			elif access <= 20: self.irc.send('MODE %s %sh %s' % (chan, sign, nick))
+			elif access <= 30: self.irc.send('MODE %s %sv %s' % (chan, sign, nick))
+		return
+
+	if users.is_owner(self, mask): access = 0
+	else: access = users.get_chan_access(self, chan, mask)
+	if access > 20: return None
 	
 	if arr['args'] is not None and len(arr['args']): args = arr['args']
 	else: args = [ arr['nick'] ]
 	args = [ a for a in args if len(a) ]
 	cmd = arr['command']
-	chan = arr['chan']
 
 	if cmd == 'topic':
 		if len(args): self.irc.socket.send('TOPIC %s :%s\n' % (chan, ' '.join(args)))
@@ -38,11 +50,11 @@ def action_opmodes(arr):
 
 	sign = '-' if cmd.startswith('de') else '+'
 
-	if cmd.endswith('op'): mode = 'o'
+	if cmd.endswith('op') and access <= 10: mode = 'o'
+	elif cmd.endswith('ban') and access <= 10: mode = 'b'
 	elif cmd.endswith('voice'): mode = 'v'
-	elif cmd.endswith('ban'): mode = 'b'
+	else: return
 
 	modes = mode*len(args)
 	nicks = ' '.join(args)
 	self.irc.socket.send('MODE %s %s%s %s\n' % (chan, sign, modes, nicks))
-	return None
